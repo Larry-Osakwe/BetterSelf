a7dd2d31fb010c1ec690fa6d99ce67fa86594964
Larry <larryosak@gmail.com>
2017-06-09 16:05:48 -0700

    Implements two imageViews with intents to new activites

--- app/src/main/java/com/larry/osakwe/betterself/MainActivity.java
+++ app/src/main/java/com/larry/osakwe/betterself/MainActivity.java
@@ -1,7 +1,10 @@
 package com.larry.osakwe.betterself;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView sleepImage = (ImageView) findViewById(R.id.sleep_image);
+        //Set a click listener on that view
+        sleepImage.setOnClickListener(new View.OnClickListener() {
+            //The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent sleepIntent = new Intent(MainActivity.this, SleepActivity.class);
+                startActivity(sleepIntent);
+            }
+        });
+
+        ImageView eatImage = (ImageView) findViewById(R.id.eat_image);
+        //Set a click listener on that view
+        eatImage.setOnClickListener(new View.OnClickListener() {
+            //The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent eatIntent = new Intent(MainActivity.this, EatActivity.class);
+                startActivity(eatIntent);
+            }
+        });
+
     }
 }
