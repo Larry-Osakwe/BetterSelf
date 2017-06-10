670688aa1aca39bfbb81eb35572873ffdf89a0b3
Larry <larryosak@gmail.com>
2017-06-09 18:58:24 -0700

    Start building TDEE calc logic

--- app/src/main/java/com/larry/osakwe/betterself/EatActivity.java
+++ app/src/main/java/com/larry/osakwe/betterself/EatActivity.java
@@ -1,13 +1,93 @@
 package com.larry.osakwe.betterself;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import static com.larry.osakwe.betterself.R.id.tdee;
 
 public class EatActivity extends AppCompatActivity {
+
+    private int age;
+    private double feet;
+    private double inches;
+    private double weight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eat);
+
+        Button calcButton = (Button) findViewById(R.id.calculate);
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayTDEE(""+ bmrCalc());
+            }
+        });
+
+
     }
+
+
+    private double bmrCalc() {
+        RadioGroup radioGroupCity = (RadioGroup) findViewById(R.id.male_or_female);
+        RadioButton trueCheckRadioButton = (RadioButton) radioGroupCity.findViewById(radioGroupCity.getCheckedRadioButtonId());
+        boolean checked = (trueCheckRadioButton.isChecked());
+
+        switch (trueCheckRadioButton.getId()) {
+            case R.id.male:
+                if (checked) {
+                    return menBMR(age(), 5, 7, 175);
+
+                }
+            case R.id.female:
+                if (checked) {
+                    return women(age(), 5, 7, 175);
+                }
+        }
+
+        return -1;
+    }
+
+
+
+
+
+    private void displayTDEE(String tdeeCalc) {
+        TextView tdeeView = (TextView) findViewById(tdee);
+        tdeeView.setText("" + tdeeCalc);
+    }
+
+
+    private double menBMR(int age, double feet, double inches, double weight) {
+        return 66 + (6.2 * weight) + (12.7 * ((12*feet)+inches)) - (6.76 * age);
+    }
+
+    private double women(int age, double feet, double inches, double weight) {
+        return 665.1 + (4.35 * weight) + (4.7 * ((12*feet)+inches)) - (4.7 * age);
+    }
+
+    private int age() {
+        EditText ageField = (EditText) findViewById(R.id.age);
+        int finalValue = Integer.parseInt(ageField.getText().toString());
+        return finalValue;
+    }
+
+    private double height() {
+        EditText feetField = (EditText) findViewById(R.id.age);
+        int feetValue = Integer.parseInt(feetField.getText().toString());
+
+        EditText inchField = (EditText) findViewById(R.id.age);
+        int inchValue = Integer.parseInt(inchField.getText().toString());
+
+        return 1;
+    }
+
+
 }
