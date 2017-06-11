6d5d4b37078d47f50db3558acdb7b616f2b69eb1
Larry <larryosak@gmail.com>
2017-06-11 09:28:31 -0700

    Begin constructing sleepActivity layout

--- app/src/main/java/com/larry/osakwe/betterself/SleepActivity.java
+++ app/src/main/java/com/larry/osakwe/betterself/SleepActivity.java
@@ -1,7 +1,10 @@
 package com.larry.osakwe.betterself;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class SleepActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class SleepActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
+
+        Calendar cal = Calendar.getInstance();
+
+
+        TextView test = (TextView)findViewById(R.id.test);
+        test.setText(""+cal.get(Calendar.HOUR_OF_DAY));
+
+
     }
 }
